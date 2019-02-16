fbaecee804a174d7493c02520c4af475c7ce958e
justin <justin.fournier@epitech.eu>
2019-02-16 19:39:42 +0100

    comment event in index.js

--- app/index.js
+++ app/index.js
@@ -25,8 +25,7 @@ function newConnectionHandler (req, res) {
 }
 
 /**
- * On client connection, get the list of all games, and
- * define all reactions to the different events which can be triggered by the client
+ * When a client has just connected, send the list of all created games to it
  */
 io.on('connection', function (socket) {
   db.getAllGames()
@@ -35,6 +34,10 @@ io.on('connection', function (socket) {
         socket.emit('newGame', { id: game.id, state: game.state, player1: game.players[0], player2: game.players[1] })
       }
     })
+  /**
+   * When a client create a new game, add it to the database, and broadcast the info to all clients
+   */
+
   socket.on('newGame', data => {
     let player1Name = data.player1
     let player2Name = data.player2
@@ -43,12 +46,21 @@ io.on('connection', function (socket) {
         io.sockets.emit('newGame', { id: data[0].id, state: true, player1: player1Name, player2: player2Name })
       })
   })
+  /**
+   * When a client delete a game, delete it from the database, and broadcast the info to all clients
+   */
+
   socket.on('deleteGame', data => {
     db.deleteGame(data)
       .then((data) => {
         io.sockets.emit('deleteGame', data[0].id)
       })
   })
+
+  /**
+   * When a client enable/disable a game, update the game state in database and
+   * broadcast the change to all clients except the one which triggered the event
+   */
   socket.on('toggleGame', data => {
     db.updateGameState(data)
       .then((data) => {
