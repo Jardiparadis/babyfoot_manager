bb4d75a20af4758de71be4203c56b84ad67dd947
justin <justin.fournier@epitech.eu>
2019-02-17 02:39:53 +0100

    add some tests Fix the display of number of active games

--- test/startup.js
+++ test/startup.js
@@ -2,7 +2,6 @@
 
 const assert = require('assert')
 const database = require('../app/database')
-let gameId = null
 
 before(() => {
   database.connectToDatabase()
@@ -12,23 +11,65 @@ describe('Database', () => {
   it('Create a new game', () => {
     return database.createNewGame('player_1_name', 'player_2_name')
       .then((data) => {
-        gameId = data[0].id
         return database.database.any(`SELECT * FROM "Game" WHERE id=${data[0].id}`)
           .then((data) => {
             assert.strictEqual(data[0].players[0], 'player_1_name')
             assert.strictEqual(data[0].players[1], 'player_2_name')
             assert.strictEqual(data[0].state, true)
+            return database.database.one(`DELETE FROM "Game" WHERE id=${data[0].id} RETURNING id;`)
           })
       })
   })
 
   it('Update game state', () => {
-    return database.updateGameState(gameId)
+    let gameId = null
+    return database.createNewGame('player_1_name', 'player_2_name')
       .then((data) => {
-        return database.database.one(`SELECT "state" FROM "Game" WHERE id=${data[0].id}`)
+        gameId = data[0].id
+        return database.updateGameState(data[0].id)
           .then((data) => {
-            assert.strictEqual(data.state, true)
+            return database.database.one(`SELECT "state" FROM "Game" WHERE id=${data[0].id}`)
+              .then((data) => {
+                assert.strictEqual(data.state, false)
+                return database.database.one(`DELETE FROM "Game" WHERE id=${gameId} RETURNING id;`)
+              })
           })
       })
+  })
+
+  it('Get all games', () => {
+    return database.createNewGame('player_all_1_name', 'player_all_2_name')
+      .then(() => {
+        return database.createNewGame('player_all_3_name', 'player_all_4_name')
+          .then(() => {
+            return database.getAllGames()
+              .then((data) => {
+                assert.strictEqual(data[0].players[0], 'player_all_1_name')
+                assert.strictEqual(data[0].players[1], 'player_all_2_name')
+                assert.strictEqual(data[1].players[0], 'player_all_3_name')
+                assert.strictEqual(data[1].players[1], 'player_all_4_name')
+                return database.database.any(`DELETE FROM "Game";`)
+              })
+          })
+      })
+  })
+
+  it('Delete game', () => {
+    let gameId = null
+    return database.createNewGame('player_1_name', 'player_2_name')
+      .then((data) => {
+        gameId = data[0].id
+        return database.deleteGame(gameId)
+          .then(() => {
+            return database.database.one('SELECT COUNT(*) FROM "Game"')
+              .then((nbEntries) => {
+                assert.strictEqual(parseInt(nbEntries.count), 0)
+              })
+          })
+      })
+  })
+
+  after(() => {
+    return database.database.any(`DELETE FROM "Game";`)
   })
 })
